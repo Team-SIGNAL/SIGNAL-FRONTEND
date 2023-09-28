1d9df87996bd76676956bc8bb829bc208fc69cff
kimgyuha <kimgyuha25@gmail.com>
2023-09-28 21:33:03 +0900

    ⚡️ :: color & text  추가

---
+++ src/styles/color.ts
@@ -0,0 +1,21 @@
+const Color = {
+  primary: {
+    100: "#00E252",
+    200: "#43CB74",
+    300: "#3CB066",
+    400: "#529959",
+    500: "#4E8C55",
+  },
+  gray: {
+    100: "#FAFAFA",
+    200: "#F7F7F7",
+    300: "#E5E5E5",
+    400: "#CCCCCC",
+    500: "#7F7F7F",
+    600: "#444444",
+  },
+  white: "#ffffff",
+  black: "#000000",
+};
+
+export default Color;
