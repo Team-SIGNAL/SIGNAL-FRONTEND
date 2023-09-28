e19ff04c6d029d7be2630be5161055dd626e7c1f
kimgyuha <kimgyuha25@gmail.com>
2023-09-28 21:32:35 +0900

    ⚡️ :: header  추가

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,17 @@
+import Logo from "assets/logo";
+import * as S from "./style";
+import { BodyLarge } from "styles/text";
+
+function Header() {
+  return (
+    <S.Container>
+      <Logo />
+      <S.Menu>
+        <BodyLarge>병원 로그인</BodyLarge>
+        <BodyLarge>병원 등록하기</BodyLarge>
+      </S.Menu>
+    </S.Container>
+  );
+}
+
+export default Header;
---
+++ src/components/common/Header/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Color from "styles/color";
+
+export const Container = styled.div`
+  width: 100vw;
+  padding: 10px 10vw;
+  background-color: ${Color.white};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+`
+export const Menu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  color: ${Color.gray[500]};
+  >p{
+    cursor: pointer;
+  }
+`
